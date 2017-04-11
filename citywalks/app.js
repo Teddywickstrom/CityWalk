00fa4c26a1f5fca561bfee45292dac8ae2d0d76b
MathiasJFejes <mathias.johannes.fejes@gmail.com>
2017-04-11 16:23:07 +0200

    Add express, dynamic PORT

--- citywalks/app.js
+++ citywalks/app.js
@@ -9,6 +9,7 @@ const rest = require('feathers-rest');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const service = require('feathers-mongoose');
+const express = require('express');           //Add express
 
 // Tell mongoose to use native promises
 // See http://mongoosejs.com/docs/promises.html
@@ -17,7 +18,7 @@ mongoose.Promise = global.Promise;
 const Message = require('./models/messages');
 const Attraction = require('./models/attraction');
 const City = require('./models/city');
-const User = require('./models/users');
+const User = require('./models/users');    
 
 //Connect to mongoose
 mongoose.connect('mongodb://masj:Mint.js1337@ds050539.mlab.com:50539/citywalks');
@@ -31,6 +32,10 @@ const app = feathers()
   .use(bodyParser.json())
   // Turn on URL-encoded parser for REST services
   .use(bodyParser.urlencoded({extended: true}))
+
+// Get the PORT
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 // Connect to the db, create and register a Feathers service.
 
@@ -70,5 +75,7 @@ app.use('/api/attractions', service({
   }
 }));
 
-app.listen(3000);
-console.log('Running on port 3000...');
+//Listen on PORT defined above
+app.listen(app.get('port'), function() {
+ console.log("Node app is running at localhost:" + app.get('port'))
+})
